8a13f9f291ccc36746b361e86a9a7760a0c48bde
Kanakosaitoh <kanahanasasha@gmail.com>
2025-04-02 12:49:37 -0700

    Created the folder and deleted some files.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather />
-      </div>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,22 @@
 import React from "react";
 export default function Weather() {
-  return "hi";
+  return (
+    <div className="container">
+      <h1>Burnaby, Canada</h1>
+      <h3>Mostly Cloudy, 12:10 PM</h3>
+      <div className="row">
+        <div className="col-6">
+          <span>icon 10 °F | °C </span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 49% </li>
+            <li>Humidity: 73%</li>
+            <li>Wind: 3 km/h</li>
+            <li>UV Index: 1</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
